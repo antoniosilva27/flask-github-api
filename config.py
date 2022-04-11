cd106aaa0fb68396ddc6fb5012654309f7478144
Antonio Silva <antoniosergio21@hotmail.com>
2022-04-11 15:11:03 -0300

    static added

--- config.py
+++ config.py
@@ -2,6 +2,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
 
 app = Flask(__name__, template_folder='template')
+app.secret_key = "super secret key"
 app.config ['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///github.sqlite3'
 
 db = SQLAlchemy(app)
